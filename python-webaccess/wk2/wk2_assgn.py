879e86e06d3a39b9e1daa0010353a8c209fb71d2
Charles Milam <charles.milam@gmail.com>
2015-10-31 20:13:53 -0500

    Add comments.

--- python-webaccess/wk2/wk2_assgn.py
+++ python-webaccess/wk2/wk2_assgn.py
@@ -4,13 +4,16 @@ inputFile = open('regex_sum_189847.txt', 'r')
 strFile = ''
 sumNums = 0
 
+# iterate through the input file and append each line to string
 for line in inputFile:
     strFile = strFile + line
 
+# find all occurances of digits within the stringified file
 nums = re.findall('[0-9]+', strFile)
 
 print 'Elements in nums: ' + str(len(nums))
 
+# iterate through the list of nums and and sum them
 for num in nums:
     sumNums += int(num)
 
