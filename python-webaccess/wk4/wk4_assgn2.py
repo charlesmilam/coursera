5e3132ee43b0a65e3bcedbd2bcca35b0ceb3c48f
Charles Milam <charles.milam@gmail.com>
2015-11-09 21:55:06 -0600

    Add function to handle retrieval of url. Returns a BeautifulSoup object. Add code to loop through the bs data the requested number of times. following the link found at the provided position.

--- python-webaccess/wk4/wk4_assgn2.py
+++ python-webaccess/wk4/wk4_assgn2.py
@@ -1,15 +1,20 @@
+import re
 import urllib2
 from bs4 import BeautifulSoup
 
-response = urllib2.urlopen("http://pr4e.dr-chuck.com/tsugi/mod/python-data/data/known_by_Fikret.html")
+def get_bs(link):
+    response = urllib2.urlopen(link)
+    html = response.read()
+    return BeautifulSoup(html)
 
-html = response.read()
-bs = BeautifulSoup(html)
-total = 0
-link_pos = 3
+counter = 1
+link_pos = 2
 num_iters = 4
 
-for tag in bs.find_all('a'):
-    print tag['href']
+bs = get_bs("http://pr4e.dr-chuck.com/tsugi/mod/python-data/data/known_by_Fikret.html")
 
-# print total
+while counter <= num_iters:
+    next_link = str(bs.find_all('a')[link_pos]['href'])
+    print next_link
+    bs = get_bs(str(next_link))
+    counter += 1
