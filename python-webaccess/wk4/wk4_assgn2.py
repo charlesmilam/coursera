249a1ca9b18c4460a58c019bccf12d81ce45e4dc
Charles Milam <charles.milam@gmail.com>
2015-11-09 21:14:08 -0600

    Add call to bs4. Modify loop to use find_all instead of straight call to the tag type.

--- python-webaccess/wk4/wk4_assgn2.py
+++ python-webaccess/wk4/wk4_assgn2.py
@@ -1,17 +1,15 @@
 import urllib2
-import BeautifulSoup
-
-BS = BeautifulSoup.BeautifulSoup
+from bs4 import BeautifulSoup
 
 response = urllib2.urlopen("http://pr4e.dr-chuck.com/tsugi/mod/python-data/data/known_by_Fikret.html")
 
 html = response.read()
-bs = BS(html)
+bs = BeautifulSoup(html)
 total = 0
 link_pos = 3
 num_iters = 4
 
-for tag in bs("a"):
-    print tag
+for tag in bs.find_all('a'):
+    print tag['href']
 
 # print total
