4779d7536219bbfdd14ee7c2f756a3440de01d36
Charles Milam <charles.milam@gmail.com>
2015-11-09 20:18:39 -0600

    Import BeautifulSoup. Pass the sample file html body, retrieved via urllib2 to BeautifulSoup. Test print title of html from BeautifulSoup.

--- python-webaccess/wk4/wk4_assgn.py
+++ python-webaccess/wk4/wk4_assgn.py
@@ -1,10 +1,15 @@
 import urllib2
+import BeautifulSoup
+
+BS = BeautifulSoup.BeautifulSoup
 
 response = urllib2.urlopen("http://pr4e.dr-chuck.com/tsugi/mod/python-data/data/comments_42.html")
 info = response.info()
 html = response.read()
+bs = BS(html)
 
 # print the response headers
 print info
 # print the response body
 print html
+print bs.title
