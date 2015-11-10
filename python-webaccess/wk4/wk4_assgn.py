bd74734ecdfbd5e75116b48c822cc5a52689f415
Charles Milam <charles.milam@gmail.com>
2015-11-09 20:51:35 -0600

    Modify url for pulling data to use that which is required for grading. Modify loop of bs data to pull the text (numeric) value out of the tag. Add its value to the accumulator. Print the accumulator.

--- python-webaccess/wk4/wk4_assgn.py
+++ python-webaccess/wk4/wk4_assgn.py
@@ -3,12 +3,13 @@ import BeautifulSoup
 
 BS = BeautifulSoup.BeautifulSoup
 
-response = urllib2.urlopen("http://pr4e.dr-chuck.com/tsugi/mod/python-data/data/comments_42.html")
-info = response.info()
+response = urllib2.urlopen("http://pr4e.dr-chuck.com/tsugi/mod/python-data/data/comments_189852.html")
+
 html = response.read()
 bs = BS(html)
-
-print bs("span")
+total = 0
 
 for tag in bs("span"):
-    print tag.name
+    total += int(tag.text)
+
+print total
