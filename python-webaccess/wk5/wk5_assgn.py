2cbfc8f849986956fbce9fbf2ce88ba2a24ae0c7
Charles Milam <charles.milam@gmail.com>
2015-11-16 21:24:52 -0600

    Add code to retrieve the count value from the comment elements, and add to accumulator. Add printing of number of comments elements and the sum of the counts.

--- python-webaccess/wk5/wk5_assgn.py
+++ python-webaccess/wk5/wk5_assgn.py
@@ -4,11 +4,14 @@ import xml.etree.ElementTree as ET
 response = urllib2.urlopen('http://pr4e.dr-chuck.com/tsugi/mod/python-data/data/comments_42.xml')
 
 xml = response.read()
-# print xml
-# print type(xml)
 tree = ET.fromstring(xml)
+
+total = 0
 
 comments = tree.findall(".//comment")
 print 'number of comments:', len(comments)
+
 for comment in comments:
-    print comment.get('name')
+    total += int(comment.find('count').text)
+
+print 'sum of counts:', total
