1c2c1bb9bae5324563b8357a83155e4b19da4a57
Charles Milam <charles.milam@gmail.com>
2015-11-19 22:59:04 -0600

    Build query url, from the provided base url, and the url encoded query params. Grab data from api endpoint built from query url. Convert raw string data to json, and extract requested place id, and print it.

---
+++ python-webaccess/wk6/output.js
@@ -0,0 +1,71 @@
+
+{
+   "results" : [
+      {
+         "address_components" : [
+            {
+               "long_name" : "South Federal Boulevard",
+               "short_name" : "S Federal Blvd",
+               "types" : [ "route" ]
+            },
+            {
+               "long_name" : "South",
+               "short_name" : "South",
+               "types" : [ "neighborhood", "political" ]
+            },
+            {
+               "long_name" : "Denver",
+               "short_name" : "Denver",
+               "types" : [ "locality", "political" ]
+            },
+            {
+               "long_name" : "Denver County",
+               "short_name" : "Denver County",
+               "types" : [ "administrative_area_level_2", "political" ]
+            },
+            {
+               "long_name" : "Colorado",
+               "short_name" : "CO",
+               "types" : [ "administrative_area_level_1", "political" ]
+            },
+            {
+               "long_name" : "United States",
+               "short_name" : "US",
+               "types" : [ "country", "political" ]
+            }
+         ],
+         "formatted_address" : "S Federal Blvd, Denver, CO, USA",
+         "geometry" : {
+            "bounds" : {
+               "northeast" : {
+                  "lat" : 39.7133974,
+                  "lng" : -105.0209967
+               },
+               "southwest" : {
+                  "lat" : 39.6318008,
+                  "lng" : -105.0279356
+               }
+            },
+            "location" : {
+               "lat" : 39.6726798,
+               "lng" : -105.0250535
+            },
+            "location_type" : "GEOMETRIC_CENTER",
+            "viewport" : {
+               "northeast" : {
+                  "lat" : 39.7133974,
+                  "lng" : -105.0209967
+               },
+               "southwest" : {
+                  "lat" : 39.6318008,
+                  "lng" : -105.0279356
+               }
+            }
+         },
+         "partial_match" : true,
+         "place_id" : "ChIJJ8oO7_B_bIcR2AlhC8nKlok",
+         "types" : [ "route" ]
+      }
+   ],
+   "status" : "OK"
+}
--- python-webaccess/wk6/wk6_assgn2.py
+++ python-webaccess/wk6/wk6_assgn2.py
@@ -5,17 +5,17 @@ import json
 location = 'South Federal University'
 base_url = 'http://pr4e.dr-chuck.com/tsugi/mod/python-data/data/geojson?'
 query_url = base_url + urllib.urlencode({'sensor':'false', 'address': location})
-print query_url
+
 response = urllib2.urlopen(query_url)
-#
-# jdata = response.read()
-# data = json.loads(jdata)
-#
-# total = 0
-#
-# for datum in data['comments']:
-#     total += datum['count']
-#
-# print 'number of comments:', len(data['comments'])
-#
-# print 'sum of counts:', total
+
+data = response.read()
+
+try:
+    jdata = json.loads(data)
+except:
+    jdata = None
+    if 'status' not in jdata or jdata['status'] != 'OK':
+        print '==== Failure To Retrieve ===='
+        print data
+
+print jdata['results'][0]['place_id']
