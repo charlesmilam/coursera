bc23a866391bbf49b887e86364e9744e2905e964
Charles Milam <charles.milam@gmail.com>
2015-11-19 22:13:32 -0600

    Clean up comments.

--- python-webaccess/wk6/wk6_assgn1.py
+++ python-webaccess/wk6/wk6_assgn1.py
@@ -1,7 +1,7 @@
 import urllib2
 import json
 
-response = urllib2.urlopen('http://pr4e.dr-chuck.com/tsugi/mod/python-data/data/comments_42.json')
+response = urllib2.urlopen('http://pr4e.dr-chuck.com/tsugi/mod/python-data/data/comments_189853.json')
 
 jdata = response.read()
 data = json.loads(jdata)
@@ -10,10 +10,7 @@ total = 0
 
 for datum in data['comments']:
     total += datum['count']
-# comments = tree.findall(".//comment")
+
 print 'number of comments:', len(data['comments'])
-#
-# for comment in comments:
-#     total += int(comment.find('count').text)
-#
+
 print 'sum of counts:', total
