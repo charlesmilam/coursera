5c41eae9f142a99687b249c7866c96973082aa6b
Charles Milam <charles.milam@gmail.com>
2015-11-28 18:25:43 -0600

    Move collection creation from siteace.js to collections.js in shared folder.

--- meteor-ulondon/wk4/siteace.js
+++ meteor-ulondon/wk4/siteace.js
@@ -1,4 +1,4 @@
-Websites = new Mongo.Collection("websites");
+// Websites = new Mongo.Collection("websites");
 
 if (Meteor.isClient) {
 
@@ -61,35 +61,35 @@ if (Meteor.isClient) {
 
 
 if (Meteor.isServer) {
-	// start up function that creates entries in the Websites databases.
-  Meteor.startup(function () {
-    // code to run on server at startup
-    if (!Websites.findOne()){
-    	console.log("No websites yet. Creating starter data.");
-    	  Websites.insert({
-    		title:"Goldsmiths Computing Department",
-    		url:"http://www.gold.ac.uk/computing/",
-    		description:"This is where this course was developed.",
-    		createdOn:new Date()
-    	});
-    	 Websites.insert({
-    		title:"University of London",
-    		url:"http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route",
-    		description:"University of London International Programme.",
-    		createdOn:new Date()
-    	});
-    	 Websites.insert({
-    		title:"Coursera",
-    		url:"http://www.coursera.org",
-    		description:"Universal access to the world’s best education.",
-    		createdOn:new Date()
-    	});
-    	Websites.insert({
-    		title:"Google",
-    		url:"http://www.google.com",
-    		description:"Popular search engine.",
-    		createdOn:new Date()
-    	});
-    }
-  });
+	// // start up function that creates entries in the Websites databases.
+  // Meteor.startup(function () {
+  //   // code to run on server at startup
+  //   if (!Websites.findOne()){
+  //   	console.log("No websites yet. Creating starter data.");
+  //   	  Websites.insert({
+  //   		title:"Goldsmiths Computing Department",
+  //   		url:"http://www.gold.ac.uk/computing/",
+  //   		description:"This is where this course was developed.",
+  //   		createdOn:new Date()
+  //   	});
+  //   	 Websites.insert({
+  //   		title:"University of London",
+  //   		url:"http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route",
+  //   		description:"University of London International Programme.",
+  //   		createdOn:new Date()
+  //   	});
+  //   	 Websites.insert({
+  //   		title:"Coursera",
+  //   		url:"http://www.coursera.org",
+  //   		description:"Universal access to the world’s best education.",
+  //   		createdOn:new Date()
+  //   	});
+  //   	Websites.insert({
+  //   		title:"Google",
+  //   		url:"http://www.google.com",
+  //   		description:"Popular search engine.",
+  //   		createdOn:new Date()
+  //   	});
+  //   }
+  // });
 }
