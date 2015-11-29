26810a02d51693d6ee6db9049f4d8bcf94318374
Charles Milam <charles.milam@gmail.com>
2015-11-28 22:18:09 -0600

    Add ability to save form data from add website form to collection.

--- meteor-ulondon/wk4/client/main.js
+++ meteor-ulondon/wk4/client/main.js
@@ -26,7 +26,7 @@ Template.website_item.events({
     // example of how you can access the id for the website in the database
     // (this is the data context for the template)
     var website_id = this._id;
-    console.log("Up voting website with id "+website_id);
+    console.log("Up voting website with id "+ website_id);
     // put the code in here to add a vote to a website!
 
     return false;// prevent the button from reloading the page
@@ -36,7 +36,7 @@ Template.website_item.events({
     // example of how you can access the id for the website in the database
     // (this is the data context for the template)
     var website_id = this._id;
-    console.log("Down voting website with id "+website_id);
+    console.log("Down voting website with id "+ website_id);
 
     // put the code in here to remove a vote from a website!
 
@@ -49,12 +49,20 @@ Template.website_form.events({
     $("#website_form").toggle('slow');
   },
   "submit .js-save-website-form":function(event){
-
-    // here is an example of how to get the url out of the form:
+    console.log('save event:', event);
+    // get the data out of the form:
+    var title = event.target.title.value;
     var url = event.target.url.value;
-    console.log("The url they entered is: "+url);
+    var description = event.target.description.value;
+    console.log("The url they entered is: "+ url);
 
-    //  put your website saving code in here!
+    // website saving code
+    Websites.insert({
+      title: title,
+      url: url,
+      description: description,
+      createdOn:new Date()
+    });
 
     return false;// stop the form submit from reloading the page
 
