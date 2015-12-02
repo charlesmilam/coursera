64a0fcc2dce6a7ea184b83dc72c2887b9e952186
Charles Milam <charles.milam@gmail.com>
2015-12-02 00:27:14 -0600

    Modify search template so that it uses the websites list template. Modified the layout template to no longer use the website list template. Basically this replaced the original layout with the use of the search layout as the main way to display the website list. The list can then be filtered or searched on by the user.

--- meteor-ulondon/wk4/lib/collections.js
+++ meteor-ulondon/wk4/lib/collections.js
@@ -3,7 +3,12 @@ Websites = new Mongo.Collection("websites");
 // index for use by easy search
 WebsitesIndex = new EasySearch.Index({
   collection: Websites,
-  fields: ['url', 'title', 'description'],
+  fields: ['username', 'title', 'description'],
   limit: 5,
-  engine: new EasySearch.Minimongo()
+  engine: new EasySearch.Minimongo({
+    sort: function()
+      {
+        return {'upVote': -1, 'title': 1 };
+      }
+  })
 });
